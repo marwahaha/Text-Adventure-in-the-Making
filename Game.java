2ae1c946576f88269f5b6a8c9396b02fdb55b7ab
Sirilean <41452204+Sirilean@users.noreply.github.com>
2018-07-20 15:05:22 -0400

    Add files via upload

--- Game.java
+++ Game.java
@@ -13,38 +13,11 @@ public class Game{
       System.out.println("1. Start");
       System.out.println("2. Reset");
       System.out.println("3. Quit");
-      
       menuChoice = sc.nextInt();
       switch(menuChoice){
         case 1:
           System.out.println("Starting game..");
-          //player choice
-          Scanner sc2 = new Scanner(System.in);
-          System.out.println("Enter your name: ");
-          String name = sc.next();
-          System.out.println("Choose your class:");
-          System.out.println("1: Warrior");
-          System.out.println("2: Rogue");
-          System.out.println("3: Wizard");
-          int choice;
-          choice = sc2.nextInt();
-          Player p;
-          switch(choice){
-            case 1:
-              System.out.println("Welcome, Warrior "+ name +".");
-              p = new Player(name, "Warrior");
-              break;
-            case 2:
-              System.out.println("Welcome, Rogue "+ name +".");
-              p = new Player(name, "Rogue");
-              break;
-            case 3:
-              System.out.println("Welcome, Wizard "+ name +".");
-              p = new Player(name, "Wizard");
-              break;
-            default:
-              System.out.println("Invalid Choice");
-          }
+          Player p = new Player();
           //start game
           
           break;
--- Player.java
+++ Player.java
@@ -1,6 +1,8 @@
+import java.util.Scanner; 
+
 public class Player{
   //general
-  private String name;
+  //private String name;
   private String gameClass;
   private int lvl;
   private int hp;
@@ -13,37 +15,69 @@ public class Player{
   private int mag; //mana & magic damage
   private int lck; //crit & magic find(?)
   
-  Player(String name, String gameClass){
-    this.name = name;
-    this.gameClass = gameClass;
+  public Player(){
     this.lvl = 1;
     this.gold = 0;
-    if (gameClass.equals("Warrior")){ 
-      this.hp = 50;
-      this.mana = 15;
-      this.str = 5; 
-      this.def = 4;
-      this.spd = 3;
-      this.mag = 1; 
-      this.lck = 1;
+    Scanner sc2 = new Scanner(System.in);
+    int choice;
+    boolean loop = true;
+    while (loop == true) {
+      System.out.println("Choose your class:");
+      System.out.println("1: Warrior");
+      System.out.println("2: Rogue");
+      System.out.println("3: Wizard");
+      choice = sc2.nextInt();
+      switch(choice){
+        case 1:
+          System.out.println("Welcome, Warrior.");
+          this.gameClass = "Warrior";
+          this.hp = 50;
+          this.mana = 15;
+          this.str = 5; 
+          this.def = 4;
+          this.spd = 3;
+          this.mag = 1; 
+          this.lck = 1;
+          loop = false;
+          break;
+        case 2:
+          System.out.println("Welcome, Rogue.");
+          this.gameClass = "Rogue";
+          this.hp = 40;
+          this.mana = 20;
+          this.str = 3;
+          this.def = 2;
+          this.spd = 5;
+          this.mag = 1; 
+          this.lck = 2;
+          loop = false;
+          break;
+        case 3:
+          System.out.println("Welcome, Wizard.");
+          this.gameClass = "Wizard";
+          this.hp = 35;
+          this.mana = 35;
+          this.str = 2;
+          this.def = 2;
+          this.spd = 3;
+          this.mag = 5; 
+          this.lck = 1;
+          loop = false;
+          break;
+        default:
+          System.out.println("Invalid Choice");
+      }
     }
-    if (gameClass.equals("Rogue")){ 
-      this.hp = 40;
-      this.mana = 20;
-      this.str = 3;
-      this.def = 2;
-      this.spd = 5;
-      this.mag = 1; 
-      this.lck = 2;
-    }
-     if (gameClass.equals("Wizard")){ 
-      this.hp = 35;
-      this.mana = 35;
-      this.str = 2;
-      this.def = 2;
-      this.spd = 3;
-      this.mag = 5; 
-      this.lck = 1;
-    }
+  }
+  
+  public void showStats(){
+    System.out.println("HP: " + hp); 
+    System.out.println("MP: " + mana); 
+    System.out.println("Gold: " + gold); 
+    System.out.println("STR: " + str); 
+    System.out.println("DEF: " + def); 
+    System.out.println("SPD: " + spd); 
+    System.out.println("MAG: " + mag); 
+
   }
 }
